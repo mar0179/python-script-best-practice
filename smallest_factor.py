8e9ce4c8cb600d9dd49e0a4528834c1a0d72c28b
Michael Reynolds <mar0179@auburn.edu>
2026-03-02 15:36:23 -0600

    Completed first three parts of the exercise

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,19 +4,24 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
+def get_smallest_prime_factor (n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return i
+    return None
 
-x = None
-for i in range(2, n):
-    if (n % i) == 0:
-        x = i
-        break
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
-if x is None:
-    print(n)
-else:
-    print(x)
+
+    smallest_prime_factor = get_smallest_prime_factor(n)
+
+
+    if smallest_prime_factor is None:
+        print(n)
+    else:
+        print(smallest_prime_factor)
